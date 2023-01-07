34935d939dfea854c0d084030c62d9ebd5036ac8
galiciaabraham <galicia.abraham@hotmail.com>
2023-01-06 20:53:44 -0600

    Core Requirement 1 Asked the user for their grade percentage and created if statements for each grade from A to F.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the percentage of your grade? ");
+        string gradeInString = Console.ReadLine();
+        int grade = int.Parse(gradeInString);
+
+        if (grade >= 90)
+        {
+            Console.Write("Your grade is A");
+        }
+        
+        else if (grade >= 80)
+        {
+            Console.Write("Your grade is B");
+        }
+
+        else if (grade >= 70)
+        {
+            Console.Write("Your grade is C");
+        }
+
+        else if (grade >= 60)
+        {
+            Console.Write("Your grade is D");
+        }
+
+        else if (grade < 60)
+        {
+            Console.Write("Your grade is F");
+        }
     }
 }
