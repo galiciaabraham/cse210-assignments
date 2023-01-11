317715688ace5ee22ffd1cbb366ee1cad1cf4a6a
galiciaabraham <galicia.abraham@hotmail.com>
2023-01-10 20:27:36 -0600

    Core Requirements 2. Added the while loop to allow the user to keep playing until the number has been guessed.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,20 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
+        {
         Console.WriteLine("What is the magic number?");
         string magicNumberinString = Console.ReadLine();
+        int magicNumber = int.Parse(magicNumberinString);
+
+        int guessNumber = 0;
+
+        while (guessNumber != magicNumber)
+        {
+
         Console.WriteLine("What is your guess?");
         string guessNumberinString = Console.ReadLine();
+        guessNumber = int.Parse(guessNumberinString);
 
-        int magicNumber = int.Parse(magicNumberinString);
-        int guessNumber = int.Parse(guessNumberinString);
-
-        if (guessNumber == magicNumber){
-            Console.WriteLine("You guessed it!");
+        if (guessNumber > magicNumber){
+            Console.WriteLine("Lower");
         } else if (guessNumber < magicNumber){
             Console.WriteLine("Higher");
         } else {
-            Console.WriteLine("Lower");
+            Console.WriteLine("You guessed it!");
+            
         }
     }
-}
+        }}}
