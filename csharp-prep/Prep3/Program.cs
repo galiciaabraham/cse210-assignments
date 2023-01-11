3b70eb5194f948f9823f9ff4d069fb4540e4393e
galiciaabraham <galicia.abraham@hotmail.com>
2023-01-10 20:10:56 -0600

    Core Requirements 1. Created basic variables to store the input and transform it to an int type used console.write a string to let the user know if he needs to guess higher, lower or if he guessed it.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        string magicNumberinString = Console.ReadLine();
+        Console.WriteLine("What is your guess?");
+        string guessNumberinString = Console.ReadLine();
+
+        int magicNumber = int.Parse(magicNumberinString);
+        int guessNumber = int.Parse(guessNumberinString);
+
+        if (guessNumber == magicNumber){
+            Console.WriteLine("You guessed it!");
+        } else if (guessNumber < magicNumber){
+            Console.WriteLine("Higher");
+        } else {
+            Console.WriteLine("Lower");
+        }
     }
 }
