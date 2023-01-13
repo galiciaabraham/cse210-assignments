4fba3fe9d166fd6cb9397b128db8cddeaef8c817
galiciaabraham <galicia.abraham@hotmail.com>
2023-01-12 19:28:15 -0600

    PromptUserName. Added the PromptUserName function and called in within the main function, stored it in a variable within main to later on display it on the final result.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,10 +5,17 @@ class Program
     static void Main(string[] args)
     {
         DisplayWelcome();
+        string UserNameInMain = PromptUserName();
         
     }
 
     static void DisplayWelcome(){
     Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName(){
+        Console.WriteLine("Please enter your name: ");
+        string UserName = Console.ReadLine();
+        return UserName;
     }
 }
