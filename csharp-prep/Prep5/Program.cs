2ba956160facdf8cc36b1be267d0247e695ef004
galiciaabraham <galicia.abraham@hotmail.com>
2023-01-12 19:24:31 -0600

    DisplayWelcome Added the DisplayWelcome function and called it within the Main function.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        
+    }
+
+    static void DisplayWelcome(){
+    Console.WriteLine("Welcome to the program!");
     }
 }
