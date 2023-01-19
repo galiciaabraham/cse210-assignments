46a7e398edd1aeaeba6e2c04f893378784aa8fd7
galiciaabraham <galicia.abraham@hotmail.com>
2023-01-18 18:58:42 -0600

    Step 5. Added the Display method and tested it.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,12 +7,18 @@ class Program
         Job job1 = new Job ();
         job1._jobTitle = "Software Engieneer";
         job1._company = "Amazon";
-        Console.WriteLine(job1._jobTitle + " " + job1._company);
+        job1._startYear = 2020;
+        job1._endYear = 2023;
 
         Job job2 = new Job();
         job2._jobTitle = "IT Techinician";
         job2._company = "Computacenter";
-        Console.WriteLine(job2._jobTitle + " " + job2._company);
+        job2._startYear = 2016;
+        job2._endYear = 2020;
+
+        job1.Display();
+        Console.WriteLine(" ");
+        job2.Display();
 
     }
 }
