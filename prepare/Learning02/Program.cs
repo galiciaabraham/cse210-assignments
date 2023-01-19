5312e6df4f54c3a926a7867519631432bd08629f
galiciaabraham <galicia.abraham@hotmail.com>
2023-01-18 18:46:47 -0600

    Steps 1 to 4. Created tje Job class and added its respective members. Created two instances and added Console.Write statements to test the instances created on main.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job ();
+        job1._jobTitle = "Software Engieneer";
+        job1._company = "Amazon";
+        Console.WriteLine(job1._jobTitle + " " + job1._company);
+
+        Job job2 = new Job();
+        job2._jobTitle = "IT Techinician";
+        job2._company = "Computacenter";
+        Console.WriteLine(job2._jobTitle + " " + job2._company);
+
     }
 }
