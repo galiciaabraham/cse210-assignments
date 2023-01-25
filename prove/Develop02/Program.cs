81a34bbe420a756675beaf99244933a6e2c1dab8
galiciaabraham <galicia.abraham@hotmail.com>
2023-01-25 17:44:21 -0600

    Semi-Completed the Journal class. I wrote most of the methods needed for the Journal class but I messed up a little bit. I decided to save it here so I can see the progress later on github, It's ok to mess up while learning!

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        Journal journals = new Journal ();
+        string newFileName = journals.GetJournalName();
+        journals.GetEntries();
+
         Prompt prompts = new Prompt ();
         prompts._prompts = new List<string>{"What brings you joy?", "Describe a place where you felt happiest.", "What was your greatest fear, and how did you conquer it?", "Write a letter to someone that you always want to thank but have never had the chance to do so.", "What is something that you would like to change about yourself? How can you change it?", "Write a letter to someone that you care about to tell them how you feel.","Reflect and write letters to yourself with constructive feedback to improve yourself.", "I am grateful for… ...", "What would make today great? ...", "Daily affirmation.", "3 Amazing things that happened today...", "How could I have made today even better?"};
 
@@ -14,6 +18,7 @@ class Program
         entry1.GetDate();
         entry1.GetInput();
         entry1.SaveEntry();
+        entry1._fileName = newFileName;
 
 
     }
