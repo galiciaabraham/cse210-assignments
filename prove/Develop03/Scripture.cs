d36da4804ff4fc7402b35612fe118dbd16cbbaf6
galiciaabraham <galicia.abraham@hotmail.com>
2023-02-02 17:43:59 -0600

    set a getscripture function for testing.

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,25 +3,32 @@ using System.Linq;
 
 public class Scripture{
 
-    public string _scripture;
+    private string _scripture;
 
     public List<string> _wordByWord;
 
     private List<string> _scriptures;
 
+    public Scripture(){
+        _scriptures = new List<string>{"Jesus Wept", "Remember lots Wife","Adam fell that men might be; and men are, that they might have joy."};
+    }
     public Scripture(List<string>scriptures){
         _scriptures = scriptures;
+    }
+
+    public string GetScripture(){
+        return _scripture;
     }
 
     public void SplitScripture(){
         _wordByWord =  _scripture.Split(' ').ToList();
     }
 
-    public void  PickRandomScripture(List<string>scriptures){
+    public void  PickRandomScripture(){
         var random = new Random();
-       int index = random.Next(scriptures.Count);
+       int index = random.Next(_scriptures.Count);
 
-        _scripture = scriptures[index];
+        _scripture = _scriptures[index];
     }
 
 }
