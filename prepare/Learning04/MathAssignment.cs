3eb363f9e8afab7500813847d5f4c52cbc9a7e8b
galiciaabraham <galicia.abraham@hotmail.com>
2023-02-14 18:27:27 -0600

    Step 4. Created MathAssignment class

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string name,string topic,string section, string problems) : base (name, topic)
+    {
+        _textbookSection = section;
+        _problems = problems;
+    }
+    public string GetHomeworkList()
+    {
+        return $"by studying {_textbookSection}  {_problems}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -6,5 +6,8 @@ class Program
     {
         Assignment assignment1 = new Assignment("John Sloan", "Inheritance");
         Console.WriteLine(assignment1.GetSummary());
+        MathAssignment assignment2 = new MathAssignment("James Baxter","Algebra","Section 123.2","Problems 10-29");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
     }
 }
