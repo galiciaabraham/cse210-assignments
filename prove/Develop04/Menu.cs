4468bcc1f0d7e2b3588d9018b622a3cb1649310e
galiciaabraham <galicia.abraham@hotmail.com>
2023-02-17 19:08:10 -0600

    added a getter for the _quit variable in Menu

--- prove/Develop04/Menu.cs
+++ prove/Develop04/Menu.cs
@@ -4,12 +4,16 @@ public class Menu {
 
     private string _prompt;
     
-    public string _quit;
+    private string _quit;
 
 
     public Menu (){
         _prompt = "Menu Options\n1.Start breathing activity\n2.Start reflecting activity\n3.Start listing activity\n4.quit";
     
+    }
+
+    public string GetQuit(){
+        return _quit;
     }
 
     public void DisplayMenu(){
