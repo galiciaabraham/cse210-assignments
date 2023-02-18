df50ab87f1a0cf16f736d97392c9a1e189c80f98
galiciaabraham <galicia.abraham@hotmail.com>
2023-02-17 19:29:47 -0600

    added the displays attributes and methods I added the attributes and methods and tested the different displays I also created a constructor that will be used later to define what's needed.

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -4,18 +4,39 @@ public class Activity {
    private string _welcome;
    private string _instructions;
    private string _ending;
+   private string _getReady;
    private int _seconds;
    private string _spinner;
    private string _countDownNumber;
 
    public Activity(){
-    _welcome = "This is the activity tals";
+    _welcome = "Welcome ma frend";
+    _ending = "Goodbye ma frend";
+    _getReady = "Get Ready...";
+    _instructions = "these will be the instructions";
+   }
+   public Activity(string welcome, string ending, string instructions){
+    _welcome = welcome;
+    _ending = ending;
+    _instructions = instructions;
+    _getReady = "Get Ready...";
    }
 
     public void DisplayInitialMessage(){
         Console.WriteLine(_welcome);
     }
 
+    public void DisplayEndingMessage(){
+        Console.WriteLine(_ending);
+    }
+
+    public void DisplayGetReady(){
+        Console.WriteLine(_getReady);
+    }
+
+    public void DisplayInstructions(){
+        Console.WriteLine(_instructions);
+    }
     public void GetSeconds(){
         Console.Write("For how long (in seconds) would you like to perform this activity?");
        string seconds = Console.ReadLine();
