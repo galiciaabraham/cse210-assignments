8ebfb1c3ce8a6689e5659b8038802c67de9273e9
galiciaabraham <galicia.abraham@hotmail.com>
2023-02-21 18:44:58 -0600

    Modified ReflectingActivity class. I checked the example again and realized I was missing a question part I modified my methods within the class to make it reusable so I can use them for randomly picking both the question and the prompt without them repeating.

--- prove/Develop04/ReflectingActivity.cs
+++ prove/Develop04/ReflectingActivity.cs
@@ -2,49 +2,61 @@ using System;
 
 public class ReflectingActivity : Activity{
     private List<string> _prompts;
+    private List<string> _questions;
     private string _ready;
     private string _selectedPrompt;
-    private int _index;
-    private List<int> _alreadySelected = new List<int>{};
+    private string _selectedQuestion;
+    private int _indexPrompts;
+    private int _indexQuestions;
+    private List<int> _alreadySelectedPrompts = new List<int>{};
+    private List<int> _alreadySelectedQuestions = new List<int>{};
 
     public ReflectingActivity (string welcome, string ending, string instructions) : base(welcome, ending, instructions){
-        _prompts = new List<string>{"What are you greatful for","What would you do different today?","What will you do different tomorrow?","What was your great success this week?","My big idea today is...","I am here because...","I won't quit trying because..."};
+
+        _prompts = new List<string>{"Why was this experience meaningful to you?","Have you ever done anything like this before?","How did you get started?","How did you feel when it was complete?","What made this time different than other times when you were not as successful?","What is your favorite thing about this experience?","What could you learn from this experience that applies to other situations?","What did you learn about yourself through this experience?","How can you keep this experience in mind in the future?"};
+        _questions = new List<string>{"Think of a time when you stood up for someone else.","Think of a time when you did something really difficult.","Think of a time when you helped someone in need.","Think of a time when you did something truly selfless."};
     }
 
-    public string UserReady(){
+    private string UserReady(){
+        Console.WriteLine("Once you're ready hit enter:");
         return _ready = Console.ReadLine();
     }
 
-    public string PickRandomPrompt(){
+
+    private string PickRandomItem(int index, List<int> selectedList, List<string> nonSelectedList, string selected){
         //TODO: I need to set a way for the the program to know that all prompts have already been used, probably something with the _alreadyUsed list that will let me know when all index numbers in _prompts have been used.
-        if (_alreadySelected.Count != _prompts.Count){
-            return Randomness();
+        if (selectedList.Count != nonSelectedList.Count){
+            return selected = Randomness(index,selectedList,nonSelectedList,selected);
             } else{
-                _alreadySelected.Clear();
-                return Randomness();
+                selectedList.Clear();
+                return selected = Randomness(index,selectedList,nonSelectedList,selected);
             } 
 
     }  
 
-    private string Randomness(){
+    private string Randomness(int index, List<int> selectedList, List<string> nonSelectedList, string selected){
         do{
         var random = new Random();
-        _index = random.Next(_prompts.Count);
-        }while(_alreadySelected.Contains(_index));
-        _alreadySelected.Add(_index); 
-        return _selectedPrompt = _prompts[_index];
+        index = random.Next(nonSelectedList.Count);
+        }while(selectedList.Contains(index));
+        selectedList.Add(index); 
+        return selected = nonSelectedList[index];
         }
 
-    public int GetIndex(){
-        return _index;
-    }
+
 
     public void DisplayPrompt(){
+        _selectedQuestion = PickRandomItem(_indexQuestions,_alreadySelectedQuestions,_questions,_selectedQuestion);
+        Console.WriteLine(_selectedQuestion);
+        UserReady();
+        if (_ready == ""){
         for (int i = 0; i < base.GetSeconds(); i++){
-        PickRandomPrompt();
+        _selectedPrompt = PickRandomItem(_indexPrompts,_alreadySelectedPrompts,_prompts,_selectedPrompt);
         Console.WriteLine(_selectedPrompt);
+        } 
+        } else {
+            UserReady();
         }
     }
-
-
-}
+    }
+    
