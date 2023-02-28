9cb60ded26fcbefaa91c5ab452141210008f8baa
galiciaabraham <galicia.abraham@hotmail.com>
2023-02-27 18:13:12 -0600

    created list of items and tested with foreach loop

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red",4);
+        Rectangle rectangle = new Rectangle("blue",2,5);
+        Circle circle = new Circle("yellow",5);
+
+        List<Shape> shapes = new List<Shape>{};
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach(Shape shaping in shapes)
+        {
+            Console.Write(shaping.GetColor());
+            Console.WriteLine(shaping.GetArea());
+        }
     }
 }
