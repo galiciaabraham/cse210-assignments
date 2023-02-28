70ac37144f1a3227d83871a28c2e90a6b580d6b6
galiciaabraham <galicia.abraham@hotmail.com>
2023-02-27 18:05:08 -0600

    Created constructors for circle and rectangle

--- prepare/Learning05/Circle.cs
+++ prepare/Learning05/Circle.cs
@@ -4,6 +4,11 @@ public class Circle : Shape
 {
     private double _radius;
 
+    public Circle(string color, double radius) : base (color)
+    {
+        _radius = radius;
+    }
+
     public override double GetArea()
     {
         return (Math.PI * _radius)*2;
