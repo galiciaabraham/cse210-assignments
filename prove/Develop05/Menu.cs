08adbdb264acec0a70c36a2353cba96bd944f3c9
galiciaabraham <galicia.abraham@hotmail.com>
2023-03-02 18:09:06 -0600

    I made it more c# conventional

--- prove/Develop05/Menu.cs
+++ prove/Develop05/Menu.cs
@@ -1,22 +1,27 @@
 using System;
 
-public class Menu{
+public class Menu
+{
     private string _prompt;
     private string _option;
 
-    public Menu(){
+    public Menu()
+    {
         _prompt = "Menu Options\n1.Create new goal\n2.List goals\n3.Save Goals\n4.Load Goals\n5.Record Event\n6.Quit\nSelect an option fromt the menu: ";
     }
 
-    public void SetOption(string option){
+    public void SetOption(string option)
+    {
         _option = option;
     }
 
-    public string GetOption(){
+    public string GetOption()
+    {
         return _option;
     }
 
-    public void DisplayMenu(){
+    public void DisplayMenu()
+    {
         Console.Write(_prompt);
     }
 }
