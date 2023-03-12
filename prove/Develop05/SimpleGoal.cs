c546d1e00ecdcbab662430c159d678e1946b9a37
galiciaabraham <galicia.abraham@hotmail.com>
2023-03-11 18:01:17 -0600

    putting everything together

--- prove/Develop05/SimpleGoal.cs
+++ prove/Develop05/SimpleGoal.cs
@@ -5,7 +5,18 @@ public class SimpleGoal : Goal
     private string _serializedGoal;
     private List<string> _deserializedGoal = new List<string>{};
     private int _goalPoints;
-    private string toEditGoal;
+    private string _toEditGoal;
+    private string _completed = "[]";
+
+    public SimpleGoal(){
+
+    }
+/*
+    public SimpleGoal(string name, string description, string points){
+        base.SetGoalName(name);
+        base.SetGoalDescription(description);
+        base.SetGoalPoints(points);
+    }
 
 
     public override List<string> DeserializeGoal()
@@ -19,20 +30,31 @@ public class SimpleGoal : Goal
         return _deserializedGoal;
     }
 
+
     public override string SerializeGoal()
     {
         _serializedGoal = string.Join(",",_deserializedGoal);
         return _serializedGoal;
     }
+ */   
     public override void RecordEvent()
     {
         Console.Write("Which goal would you like to edit?: ");
-        toEditGoal = Console.ReadLine();
+        _toEditGoal = Console.ReadLine();
     }
     public override string CalculateScore(int score)
     {
         int scoreResult = _goalPoints += score;
         base.SetScore(scoreResult.ToString()) ;
         return base.GetScore();
+    }
+
+    public void SetSelectedGoalToEdit(string selected)
+    {
+        _toEditGoal = selected;
+    }
+    public override void MarkCompletion()
+    {
+        _completed = "[x]";
     }
 }
