3790c1ab03849d60a72aaf77377dabc059e21546
galiciaabraham <galicia.abraham@hotmail.com>
2023-03-21 18:57:39 -0500

    Tested methods returned values are correct.

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Comment> comments1 = new List<Comment>{
+        new Comment("3-21-2023","GeorgeTheFifth","amazing video!!"),
+        new Comment("3-20-2023","VodiKid","Love it, I could watch it all day long"),
+        new Comment("3-14-2023","Anonymous","Don't know how I got here but love it")
+        };
+
+        Video video1 = new Video("Monkey Spinning","Monkey",600,comments1);
+        
+
+        Console.WriteLine(video1.CommentsCount());
+        Console.WriteLine(video1.GetVideo());
+        video1.DisplayComments();
     }
 }
