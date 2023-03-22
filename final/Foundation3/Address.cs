fe74fcfc1554c23f67b4223bc4d1857b8ee18b1d
galiciaabraham <galicia.abraham@hotmail.com>
2023-03-22 18:10:04 -0500

    Copied and edited Address class from Foundation 2

---
+++ final/Foundation3/Address.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Address
+{
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _country;
+    private int _zipCode;
+
+    public Address(string street,string city, string state, string country,int zip){
+
+    _street = street;
+    _city = city;
+    _state = state;
+    _country = country;
+    _zipCode = zip;
+}
+
+    public string BuildAddress()
+    {
+        return $"{_street},\n{_city},{_state},\n{_country},\n{_zipCode}";
+    }
+
+}
+
+
