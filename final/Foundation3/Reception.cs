0a8d9226c5385fa3cf06339ac4f8d81f5be316ad
galiciaabraham <galicia.abraham@hotmail.com>
2023-03-22 18:20:25 -0500

    Added Reception class with attributes and methods

---
+++ final/Foundation3/Reception.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Reception : Event
+{
+    private string _emailAddress;
+
+    public Reception(string title, string description, string date, string time, Address address, string type, string email) : base(title,description,date,time,address,type)
+    {
+        _emailAddress = email;
+    }
+
+    public void DisplayDetails()
+    {
+        
+    }
+}
