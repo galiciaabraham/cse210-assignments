3b77834e7fb12299b0fe7a4069508a86beb7133b
galiciaabraham <galicia.abraham@hotmail.com>
2023-03-21 19:27:24 -0500

    Completed Address class and Product class.

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -3,16 +3,34 @@ using System;
 public class Address
 {
     private string _street;
+    private string _city;
+    private string _state;
     private string _country;
+    private int _zipCode;
 
-    public Address(string street, string country){
+    public Address(string street,string city, string state, string country,int zip){
+
     _street = street;
+    _city = city;
+    _state = state;
     _country = country;
+    _zipCode = zip;
 }
 
-    public string ReturnAddress()
+    public string BuildAddress()
     {
-        return $"{ _street}, {_country}";
+        return $"{_street},{_city},{_state},{_country} - {_zipCode}";
+    }
+
+    public bool CheckCountry()
+    {
+        if(_country == "US" || _country == "United States")
+        {
+            return true;
+        } else{
+            return false;
+        }
+
     }
 
 }
